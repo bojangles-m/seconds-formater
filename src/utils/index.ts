c473661c45f35d854566e1c40d8ab105b386b7c1
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-07-26 22:02:45 +0200

    updated test

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+/* (c) Copyright Bojan Mazej, all rights reserved. */
+
+export * from './addLeadingZeros';
+export * from './isNegativeNumber';
+export * from './isNumberInSeconds';
--- src/utils/isNumberInSeconds.spec.ts
+++ src/utils/isNumberInSeconds.spec.ts
@@ -3,16 +3,18 @@
 import { isNumberInSeconds } from './isNumberInSeconds';
 
 describe('isNumberInSeconds', () => {
-    it(`is positive number`, () => {
+    it(`is correct number`, () => {
         expect(isNumberInSeconds(111)).toBeTruthy();
-        expect(isNumberInSeconds(111.22)).toBeFalsy();
-        expect(isNumberInSeconds(123e22)).toBeFalsy();
+        expect(isNumberInSeconds(-123)).toBeTruthy();
+        expect(isNumberInSeconds(-12345123451234512345)).toBeTruthy();
     });
 
-    it(`is negative number`, () => {
-        expect(isNumberInSeconds(-123)).toBeTruthy();
+    it(`is not correct number`, () => {
+        expect(isNumberInSeconds(1234512345123451234512)).toBeFalsy();
+        expect(isNumberInSeconds(1234512345123451234512345)).toBeFalsy();
         expect(isNumberInSeconds(-123.22)).toBeFalsy();
         expect(isNumberInSeconds(-123e22)).toBeFalsy();
-        expect(isNumberInSeconds(-12345123451234512345)).toBeTruthy();
+        expect(isNumberInSeconds(111.22)).toBeFalsy();
+        expect(isNumberInSeconds(123e22)).toBeFalsy();
     });
 });
