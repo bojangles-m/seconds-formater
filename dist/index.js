f79320fa6e5846baaa81a83c420fd7e53e9db024
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-07-26 22:53:31 +0200

    chore: project packages

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sayMyName = void 0;
-function sayMyName(name) {
-    return name ? "You're right 👍" : "You're wrong 👎";
-}
-exports.sayMyName = sayMyName;
-sayMyName("Heisenberg");
