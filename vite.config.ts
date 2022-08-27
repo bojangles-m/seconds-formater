702a3feff39888dc2c7ccf8a7774b1bb7c34319a
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-08-27 18:33:13 +0200

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -11,23 +11,9 @@ export default defineConfig({
             entry: resolve(__dirname, 'src/index.ts'),
             fileName: (format: string) => `[name].${format}.js`,
             name: 'seconds-formatter',
+            formats: ['es', 'umd', 'cjs'],
         },
         sourcemap: true,
         minify: true,
-        rollupOptions: {
-            output: [
-                {
-                    format: 'es',
-                    preserveModules: true,
-                    preserveModulesRoot: 'src',
-                },
-                {
-                    format: 'umd',
-                },
-                {
-                    format: 'cjs',
-                },
-            ],
-        },
     },
 });
